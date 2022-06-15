2589898e8ce12a46bae97c5cf84cb3a1345dcf04
tejaskhartude <tejaskhartude@gmail.com>
2022-06-15 17:31:00 +0530

    Initial commit

---
+++ app/src/androidTest/java/calculator/Calculator.kt
@@ -0,0 +1,12 @@
+package calculator
+
+class Calculator(private val operators:Operators) {
+
+    fun addTwoNumbers(a:Int,b:Int)=operators.add(a,b)
+
+    fun subtractTwoNumbers(a:Int,b:Int) = operators.subtract(a,b)
+
+    fun multiplyTwoNumbers(a:Int,b:Int) = operators.multiply(a,b)
+
+    fun divideTwoNumbers(a:Int,b:Int) = operators.divide(a,b)
+}
---
+++ app/src/test/java/com/example/mockito/calculator/CalculatorTest.kt
@@ -0,0 +1,12 @@
+package com.example.mockito.calculator
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class CalculatorTest {
+
+    
+
+}
